06f8b7d18281ba9fe97e4c07f5d17ecdbb5e04a1
JohnLarimore <john.m.larimore@gmail.com>
2025-01-03 09:17:06 -0700

    Update config.php
    Initial set up

--- config/config.php
+++ config/config.php
@@ -1,1 +1,14 @@
+<?php
+define('DEV', true);
+//define("DOC_ROOT", '/jlrenewables/public/');
+//define("ROOT_FOLDER", 'public');
 
+//Database settings
+$type = 'mysql';
+$server = 'localhost';
+$db = 'test_db';
+$port = 3306;
+$charset = 'utf8mb4';
+$username = 'testtable_admin';
+$password = 'testtable_admin';
+$dsn = "$type:host=$server;dbname=$db;port=$port;charset=$charset";
